6d77bdbc9ab519edf1b1a8957065ce18167006ac
TPieplow <ted.pieplow@gmail.com>
2024-05-16 00:17:27 +0200

    merged Ted

--- BackOfficeSilicon/BackOfficeSilicon/Program.cs
+++ BackOfficeSilicon/BackOfficeSilicon/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 
+
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultScheme = IdentityConstants.ApplicationScheme;
