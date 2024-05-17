a44ee0197a563180fedd416a1cf7055e449e71bd
poyrakie <johan.astrom91@live.se>
2024-05-17 12:14:41 +0200

    stage

---
+++ BackOfficeSilicon/BackOfficeSilicon/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using BackOfficeSilicon.Components.Account;
+using BackOfficeSilicon.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Blazored.Modal;
+
+namespace BackOfficeSilicon.Configurations;
+
+public static class ServiceConfiguration
+{
+    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddRazorComponents()
+              .AddInteractiveServerComponents()
+              .AddInteractiveWebAssemblyComponents();
+
+        services.AddHttpClient();
+        services.AddCascadingAuthenticationState();
+        services.AddScoped<IdentityUserAccessor>();
+        services.AddScoped<IdentityRedirectManager>();
+        services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+        services.AddScoped<ClaimsPrincipal>();
+
+        services.AddBlazoredModal();
+
+        services.AddHttpContextAccessor();
+
+        services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = IdentityConstants.ApplicationScheme;
+            options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+        })
+            .AddIdentityCookies();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddRoleManager<RoleManager<IdentityRole>>()
+            .AddRoleStore<RoleStore<IdentityRole, ApplicationDbContext>>()
+            .AddDefaultTokenProviders();
+    }
+}
