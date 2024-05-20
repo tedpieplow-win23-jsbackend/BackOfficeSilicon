0e37bcb3f6151b8cc6f6276218b272ab20e977c8
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-05-20 15:30:19 +0200

    new backoffice DB added & migrated

--- BackOfficeSilicon/BackOfficeSilicon/Data/ApplicationDbContext.cs
+++ BackOfficeSilicon/BackOfficeSilicon/Data/ApplicationDbContext.cs
@@ -1,9 +1,8 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace BackOfficeSilicon.Data
+namespace BackOfficeSilicon.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
 }
--- BackOfficeSilicon/BackOfficeSilicon/Data/Migrations/20240520132841_New Db init.Designer.cs
+++ BackOfficeSilicon/BackOfficeSilicon/Data/Migrations/20240520132841_New Db init.Designer.cs
@@ -4,6 +4,7 @@ using BackOfficeSilicon.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackOfficeSilicon.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240520132841_New Db init")]
+    partial class NewDbinit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
