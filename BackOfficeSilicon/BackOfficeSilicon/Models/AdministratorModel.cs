16c53778373c63da11cb7b996ffc3f2916816f0a
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-05-21 16:01:45 +0200

    fixed role

--- BackOfficeSilicon/BackOfficeSilicon/Models/AdministratorModel.cs
+++ BackOfficeSilicon/BackOfficeSilicon/Models/AdministratorModel.cs
@@ -5,8 +5,5 @@ public class AdministratorModel
     public string Email { get; set; } = null!;
     public string FirstName { get; set; } = null!;
     public string LastName { get; set;} = null!;
-    public IEnumerable<string> Roles { get; set; } = [];
-    public string PhoneNumber { get; set; } = null!;
-    public string Biography { get; set; } = null!;
-    public string ProfileImageUrl { get; set; } = null!;
+    public string Role { get; set; } = null!;
 }
