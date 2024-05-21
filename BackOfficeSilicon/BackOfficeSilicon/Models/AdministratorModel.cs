62b768a11e16b53e4a16e758903d0d75394d8799
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-05-21 12:40:25 +0200

    admin model init

---
+++ BackOfficeSilicon/BackOfficeSilicon/Models/AdministratorModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BackOfficeSilicon.Models;
+
+public class AdministratorModel
+{
+    public string Email { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set;} = null!;
+    public IEnumerable<string> Roles { get; set; } = [];
+    public string PhoneNumber { get; set; } = null!;
+    public string Biography { get; set; } = null!;
+    public string ProfileImageUrl { get; set; } = null!;
+}
