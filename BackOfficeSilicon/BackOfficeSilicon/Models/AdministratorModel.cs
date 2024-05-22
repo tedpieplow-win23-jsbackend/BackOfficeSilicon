c7231dac02b95ea1a3fec672b64e36f3f96cb417
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-05-22 12:40:12 +0200

    admin view finished, update almost finished

--- BackOfficeSilicon/BackOfficeSilicon/Models/AdministratorModel.cs
+++ BackOfficeSilicon/BackOfficeSilicon/Models/AdministratorModel.cs
@@ -1,7 +1,12 @@
-﻿namespace BackOfficeSilicon.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackOfficeSilicon.Models;
 
 public class AdministratorModel
 {
+    [Required(ErrorMessage = "Email is required")]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid e-mail address.")]
     public string Email { get; set; } = null!;
     public string FirstName { get; set; } = null!;
     public string LastName { get; set;} = null!;
---
+++ BackOfficeSilicon/BackOfficeSilicon/Services/AdminService.cs
@@ -0,0 +1,43 @@
+﻿using BackOfficeSilicon.Data;
+using BackOfficeSilicon.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BackOfficeSilicon.Services;
+
+public class AdminService(UserManager<ApplicationUser> userManager, ILogger<AdminService> logger)
+{
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly ILogger<AdminService> _logger = logger;
+
+    public async Task<List<AdministratorModel>> PopulateAdminListAsync()
+    {
+        try
+        {
+            var adminModel = new AdministratorModel();
+            var admins = _userManager.Users.ToList();
+            var adminList = new List<AdministratorModel>();
+
+            if (admins.Count != 0)
+            {
+                foreach (var admin in admins)
+                {
+                    var userRole = await _userManager.GetRolesAsync(admin);
+
+                    adminModel.Email = admin.Email!;
+                    adminModel.FirstName = admin.FirstName;
+                    adminModel.LastName = admin.LastName;
+                    adminModel.Role = userRole.FirstOrDefault()!;
+
+                    adminList.Add(adminModel);
+                }
+            }
+
+            return adminList;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return null!;
+        }
+    }
+}
