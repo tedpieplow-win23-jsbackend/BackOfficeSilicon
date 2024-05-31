c4592bd52f486336ba2c831e9874a2bd6bdc7ebc
TPieplow <ted.pieplow@gmail.com>
2024-05-31 09:23:53 +0200

    Added validation for create course form, changed some minor styling

--- BackOfficeSilicon/BackOfficeSilicon/Models/CreateCourseCard.cs
+++ BackOfficeSilicon/BackOfficeSilicon/Models/CreateCourseCard.cs
@@ -1,32 +1,48 @@
-﻿namespace BackOfficeSilicon.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackOfficeSilicon.Models;
 
 public class CreateCourseCard
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
+    [Required(ErrorMessage = "ImageUri is required")]
     public string? ImageUri { get; set; }
+    [Required(ErrorMessage = "ImageHeaderUri is required")]
     public string? ImageHeaderUri { get; set; }
     public bool IsBestseller { get; set; }
     public bool IsDigital { get; set; }
     public string[]? Categories { get; set; }
+    [Required(ErrorMessage = "Title is required")]
     public string? Title { get; set; }
+    [Required(ErrorMessage = "Ingress is required")]
     public string? Ingress { get; set; }
+    [Required(ErrorMessage = "StarRating is required")]
     public decimal StarRating { get; set; }
+    [Required(ErrorMessage = "Reviews is required")]
     public string? Reviews { get; set; }
+    [Required(ErrorMessage = "LikesInPercent is required")]
     public string? LikesInPercent { get; set; }
+    [Required(ErrorMessage = "Likes is required")]
     public string? Likes { get; set; }
+    [Required(ErrorMessage = "Hours is required")]
     public string? Hours { get; set; }
+    [Required(ErrorMessage = "Author name is required")]
     public virtual List<AuthorCreateRequest>? Authors { get; set; }
     public virtual PricesCreateRequest? Prices { get; set; }
+    [Required(ErrorMessage = "Content is required")]
     public virtual ContentCreateRequest? Content { get; set; }
 }
 
 public class AuthorCreateRequest
 {
+    [Required(ErrorMessage = "Name is required")]
     public string? Name { get; set; }
 }
 public class ContentCreateRequest
 {
+    [Required(ErrorMessage = "Description is required")]
     public string? Description { get; set; }
+    [Required(ErrorMessage = "Includes is required")]
     public string[]? Includes { get; set; }
     public virtual List<ProgramDetailItemCreateRequest>? ProgramDetails { get; set; }
 }
@@ -41,6 +57,8 @@ public class PricesCreateRequest
 public class ProgramDetailItemCreateRequest
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required")]
     public string? Title { get; set; }
+    [Required(ErrorMessage = "Description is required")]
     public string? Description { get; set; }
 }
